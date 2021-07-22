ffed9afa0faca72787fccb01dfb4956de68ca920
jpvt <joaoteixeira@eng.ci.ufpb.br>
2021-07-22 11:39:39 -0300

    update setup for PyPi package

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
-import setuptools
+from setuptools import setup, find_packages
+import codecs
+import os
 
-setuptools.setup(
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+setup(
     name="perlin-cupy",
-    version="0.1",
+    version="0.0.1",
     author="jpvt",
     author_email="joaoteixeira@eng.ci.ufpb.br",
     description="Perlin-NumPy for GPU",
@@ -12,10 +19,13 @@ setuptools.setup(
         "Tracker": "https://github.com/jpvt/perlin-cupy/issues"
     },
     license="MIT",
-    packages=["perlin_cupy"],
+    packages=find_packages(),
     install_requires=['cupy'],
     classifiers=[
+        "Development Status :: 1 - First Version",
+        "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
+        "Operating System :: Linux",
         "License :: OSI Approved :: MIT License",
     ],
 )
